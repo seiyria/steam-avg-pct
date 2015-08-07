8f952da14e67544f0f08f78c8e7b28baffa1b8bc
Kyle Kemp <kyle@seiyria.com>
2015-08-06 20:14:51 -0500

    Update 2015-08-07T01:14:49.952Z

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-
-angular.module('SteamAvgPct', ['smart-table', 'ui.bootstrap', 'ngStorage'])
-    .filter('TableFilter', require('./filters/TableFilter'))
-    .directive('checkFilter', require('./directives/CheckFilter'))
-    .service('QueryData', require('./services/QueryData'))
-    .controller('MainCtrl', require('./controllers/MainController'))
-;
